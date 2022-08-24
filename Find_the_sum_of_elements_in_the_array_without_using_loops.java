72560d462935767b80af8c92398e45545fbdf1da
21A91A05F1 <97424376+21A91A05F1@users.noreply.github.com>
2022-08-24 13:32:03 +0530

    import java.util.Scanner; class one{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int n = sc.nextInt();         int []arr=new int[n];         int s=0;         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {             s+=arr[i];         }         System.out.println(s);     } }

---
+++ Find_the_sum_of_elements_in_the_array_without_using_loops.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class one{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n = sc.nextInt();
+        int []arr=new int[n];
+        int s=0;
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            s+=arr[i];
+        }
+        System.out.println(s);
+    }
+}
