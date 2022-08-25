041624ff240c4d5b1c5b86b56925ee293762337d
21A91A05F1 <97424376+21A91A05F1@users.noreply.github.com>
2022-08-25 20:53:59 +0530

    import java.util.Scanner; class one{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int n = sc.nextInt();         int k = sc.nextInt();         int c=0;         int []arr=new int [n];         for(int i=0;i<n;i++){             arr[i]=sc.nextInt();         }         for(int i=0;i<n;i++){             if(arr[i]%k==0){                 c+=1;             }         }         System.out.println(c);     } }

---
+++ Count_elements_divisible_by_k.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class one{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int c=0;
+        int []arr=new int [n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            if(arr[i]%k==0){
+                c+=1;
+            }
+        }
+        System.out.println(c);
+    }
+}
