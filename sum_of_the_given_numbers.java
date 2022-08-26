bbc0b3b65b03048d98f64ebc002e87ef196ab575
21A91A05F1 <97424376+21A91A05F1@users.noreply.github.com>
2022-08-26 09:31:35 +0530

    import java.util.Scanner; class one {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         for(int i=1;i<=n;i++)         {           int a=sc.nextInt();           int b=sc.nextInt();             System.out.println(a+b);         }
        }
    }

---
+++ sum_of_the_given_numbers.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class one
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+          int a=sc.nextInt();
+          int b=sc.nextInt();
+            System.out.println(a+b);
+        }
+       
+    }
+}
