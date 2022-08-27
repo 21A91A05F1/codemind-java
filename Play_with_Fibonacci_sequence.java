34d1ce40f3ce36b874c72ffb4f98b2b6182835fc
21A91A05F1 <97424376+21A91A05F1@users.noreply.github.com>
2022-08-27 21:40:32 +0530

    import java.util.Scanner; class one {     static int fib(int n){         if(n<=1){             return n;         }         else{             return fib(n-1)+fib(n-2);         }     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         for(int i=0;i<n;i++){             System.out.format("%d ",fib(i));         }
        }
    }

---
+++ Play_with_Fibonacci_sequence.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class one
+{
+    static int fib(int n){
+        if(n<=1){
+            return n;
+        }
+        else{
+            return fib(n-1)+fib(n-2);
+        }
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            System.out.format("%d ",fib(i));
+        }
+       
+       
+    }
+}
