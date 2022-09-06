fe67852362f89b052bd2a565a0573a28fa94674d
21A91A05F1 <97424376+21A91A05F1@users.noreply.github.com>
2022-09-06 18:29:35 +0530

    import java.util.Scanner; class one {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int temp=a;         int d,rev=0;         while(a!=0)         {             d=a%10;             rev=rev*10+d;             a/=10;         }         if(rev==temp)         {             System.out.println("2");         }         else         {             System.out.println("1");         }
        }
    }

---
+++ Is_Palindrome_Number?.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class one
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int temp=a;
+        int d,rev=0;
+        while(a!=0)
+        {
+            d=a%10;
+            rev=rev*10+d;
+            a/=10;
+        }
+        if(rev==temp)
+        {
+            System.out.println("2");
+        }
+        else
+        {
+            System.out.println("1");
+        }
+       
+       
+        
+       
+    }
+}
