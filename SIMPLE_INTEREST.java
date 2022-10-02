6da12911eebeb59e1bd96b067609bce874cab798
21A91A05F1 <97424376+21A91A05F1@users.noreply.github.com>
2022-10-02 21:13:32 +0530

    import java.util.Scanner; class sathwika {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int p=sc.nextInt();         int t=sc.nextInt();         int r=sc.nextInt();         int k=p*t*r;         System.out.println(k/100);
        }
    }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class sathwika
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int p=sc.nextInt();
+        int t=sc.nextInt();
+        int r=sc.nextInt();
+        int k=p*t*r;
+        System.out.println(k/100);
+       
+    }
+}
