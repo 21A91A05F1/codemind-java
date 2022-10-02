65baaa21eff37f4c6b6c48ea1304d0281976ee44
21A91A05F1 <97424376+21A91A05F1@users.noreply.github.com>
2022-10-02 20:41:22 +0530

    import java.util.Scanner; class one {     static boolean is_prime(int n){         int c=0,i=1;         while(i<=n)         {             if(n%i==0){                 c+=1;             }             i+=1;         }         if(c==2){             return true;         }         else{             return false;         }     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int []arr=new int[n];         int c=0,s=0;         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {             if(is_prime(arr[i])){                 c+=1;                 s+=arr[i];             }         }         System.out.format("%.2f",(float)s/c);     } }

---
+++ Average_of_primes.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+class one
+{
+    static boolean is_prime(int n){
+        int c=0,i=1;
+        while(i<=n)
+        {
+            if(n%i==0){
+                c+=1;
+            }
+            i+=1;
+        }
+        if(c==2){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int []arr=new int[n];
+        int c=0,s=0;
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(is_prime(arr[i])){
+                c+=1;
+                s+=arr[i];
+            }
+        }
+        System.out.format("%.2f",(float)s/c);
+    }
+}
